0c5736008ba431976b0db105e0b33ed275c776ee
Kimberly-Wangari <kimberlywangari50@gmail.com>
2025-11-20 21:32:15 +0300

    Initial Django commit

--- products/models.py
+++ products/models.py
@@ -3,11 +3,7 @@ from category.models import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        related_name="products"
-    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="products/", blank=True, null=True)
@@ -15,3 +11,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
